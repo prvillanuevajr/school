fc7cf82b4c2ed6920eaa50f5d4de38eefc7aac34
Presmelito <prvillanuevajr@gmail.com>
2020-08-16 10:38:21 +0800

    Set up migrations

---
+++ database/migrations/2020_08_16_020857_create_lectures_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLecturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lectures', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('professor_id');
+            $table->string('name');
+            $table->string('start_time');
+            $table->string('end_time');
+            $table->timestamps();
+
+            $table->foreign('professor_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lectures');
+    }
+}
---
+++ database/migrations/2020_08_16_022049_create_lecture_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLectureUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lecture_user', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('student_id');
+            $table->unsignedBigInteger('lecture_id');
+            $table->timestamps();
+
+            $table->foreign('student_id')->references('id')->on('users');
+            $table->foreign('lecture_id')->references('id')->on('lectures');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lecture_user');
+    }
+}
